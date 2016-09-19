9fcb2ac3c562a165ce4e1dfca68ee825fb8a3839
Joseph Ivie <josephivie@gmail.com>
2016-09-19 11:37:08 -0600

    Readding removed function required in PastureMap

---
+++ src/main/java/com/lightningkite/kotlin/anko/full/ImageLayouts.kt
@@ -0,0 +1,41 @@
+package com.lightningkite.kotlin.anko.full
+
+import android.view.View
+import android.view.ViewGroup
+import com.lightningkite.kotlin.anko.animation.TransitionView
+import com.lightningkite.kotlin.anko.observable.progressLayout
+import com.lightningkite.kotlin.networking.NetMethod
+import com.lightningkite.kotlin.networking.NetRequest
+import com.lightningkite.kotlin.observable.property.MutableObservableProperty
+import com.lightningkite.kotlin.observable.property.ObservableProperty
+import com.lightningkite.kotlin.observable.property.StandardObservableProperty
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.imageView
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.padding
+
+/**
+ * Various layouts that require all packages.
+ * Created by jivie on 7/15/16.
+ */
+fun ViewGroup.layoutImage(
+        urlObservable: ObservableProperty<String?>,
+        noImageResource: Int,
+        brokenImageResource: Int,
+        imageMinBytes: Long = 250 * 250 * 4,
+        downloadRequest: NetRequest = NetRequest(NetMethod.GET, ""),
+        loadingObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false),
+        setup: TransitionView.() -> Unit = {}
+): View = progressLayout(loadingObs) {
+    padding = dip(8)
+    imageView {
+        bindUrl(
+                urlObservable = urlObservable,
+                noImageResource = noImageResource,
+                brokenImageResource = brokenImageResource,
+                imageMinBytes = imageMinBytes,
+                downloadRequest = downloadRequest,
+                loadingObs = loadingObs
+        )
+    }.lparams(matchParent, matchParent)
+}.apply(setup)
