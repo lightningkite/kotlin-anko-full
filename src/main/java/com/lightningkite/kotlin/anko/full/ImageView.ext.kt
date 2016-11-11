fffb182b0429096819974f1e8839949a32e9ab4d
Joseph Ivie <josephivie@gmail.com>
2016-11-10 22:02:48 -0700

    Major updates to the example, animations, and networking

--- src/main/java/com/lightningkite/kotlin/anko/full/ImageView.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/full/ImageView.ext.kt
@@ -2,123 +2,53 @@ package com.lightningkite.kotlin.anko.full
 
 import android.net.Uri
 import android.widget.ImageView
-import com.lightningkite.kotlin.anko.imageLocalUri
 import com.lightningkite.kotlin.anko.lifecycle
-import com.lightningkite.kotlin.anko.networking.image.imageStreamExif
-import com.lightningkite.kotlin.networking.NetMethod
-import com.lightningkite.kotlin.networking.NetRequest
+import com.lightningkite.kotlin.anko.networking.image.lambdaBitmapExif
+import com.lightningkite.kotlin.async.invokeAsync
 import com.lightningkite.kotlin.observable.property.MutableObservableProperty
 import com.lightningkite.kotlin.observable.property.ObservableProperty
 import com.lightningkite.kotlin.observable.property.StandardObservableProperty
 import com.lightningkite.kotlin.observable.property.bind
-import org.jetbrains.anko.image
+import okhttp3.Request
+import org.jetbrains.anko.imageBitmap
 import org.jetbrains.anko.imageResource
-
-/**
- * Created by jivie on 7/15/16.
- */
-fun ImageView.bindUrl(
-        urlObservable: ObservableProperty<String?>,
-        noImageResource: Int? = null,
-        brokenImageResource: Int? = null,
-        imageMinBytes: Long,
-        downloadRequest: NetRequest = NetRequest(NetMethod.GET, ""),
-        loadingObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false)
-) {
-    lifecycle.bind(urlObservable) { url ->
-        if (url == null || url.isBlank()) {
-            //set to default image
-            if (noImageResource != null) {
-                imageResource = noImageResource
-            } else {
-                image = null
-            }
-        } else {
-            loadingObs.value = (true)
-            imageStreamExif(context, downloadRequest.copy(url = url), minBytes = imageMinBytes, brokenImageResource = brokenImageResource) { disposer ->
-                loadingObs.value = (false)
-                if (disposer == null) {
-                    //set to default image or broken image
-                    if (brokenImageResource != null) {
-                        imageResource = brokenImageResource
-                    } else {
-                        image = null
-                    }
-                }
-            }
-        }
-    }
-}
-
-fun ImageView.bindUrl(
-        urlObservable: ObservableProperty<String?>,
-        noImageResource: Int? = null,
-        brokenImageResource: Int? = null,
-        imageMaxWidth: Int = 2048,
-        imageMaxHeight: Int = 2048,
-        downloadRequest: NetRequest = NetRequest(NetMethod.GET, ""),
-        loadingObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false)
-) {
-    lifecycle.bind(urlObservable) { url ->
-        if (url == null || url.isBlank()) {
-            //set to default image
-            if (noImageResource != null) {
-                imageResource = noImageResource
-            } else {
-                image = null
-            }
-        } else {
-            loadingObs.value = (true)
-            imageStreamExif(
-                    context,
-                    downloadRequest.copy(url = url),
-                    maxWidth = imageMaxWidth,
-                    maxHeight = imageMaxHeight,
-                    brokenImageResource = brokenImageResource
-            ) { disposer ->
-                loadingObs.value = (false)
-                if (disposer == null) {
-                    //set to default image or broken image
-                    if (brokenImageResource != null) {
-                        imageResource = brokenImageResource
-                    } else {
-                        image = null
-                    }
-                }
-            }
-        }
-    }
-}
+import org.jetbrains.anko.imageURI
 
 fun ImageView.bindUri(
         uriObservable: ObservableProperty<String?>,
-        noImageResource: Int,
-        brokenImageResource: Int,
+        noImageResource: Int? = null,
+        brokenImageResource: Int? = null,
         imageMinBytes: Long,
-        downloadRequest: NetRequest = NetRequest(NetMethod.GET, ""),
+        requestBuilder: Request.Builder = Request.Builder(),
         loadingObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false)
 ) {
     lifecycle.bind(uriObservable) { uri ->
         if (uri == null || uri.isEmpty()) {
             //set to default image
-            imageResource = noImageResource
+            if (noImageResource != null) {
+                imageResource = noImageResource
+            }
         } else {
             val uriObj = Uri.parse(uri)
             if (uriObj.scheme.contains("http")) {
                 loadingObs.value = (true)
-                imageStreamExif(context, downloadRequest.copy(url = uri), minBytes = imageMinBytes, brokenImageResource = brokenImageResource) { disposer ->
+                requestBuilder.url(uri).lambdaBitmapExif(context, imageMinBytes).invokeAsync {
                     loadingObs.value = (false)
-                    if (disposer == null) {
+                    if (it.result == null) {
                         //set to default image or broken image
-                        imageResource = brokenImageResource
+                        if (brokenImageResource != null) {
+                            imageResource = brokenImageResource
+                        }
+                    } else {
+                        imageBitmap = it.result
                     }
                 }
             } else {
                 loadingObs.value = (true)
-                imageLocalUri(uriObj, imageMinBytes, brokenImageResource) { disposer ->
-                    loadingObs.value = (false)
-                    if (disposer == null) {
-                        //set to default image or broken image
+                try {
+                    imageURI = Uri.parse(uri)
+                } catch(e: Exception) {
+                    if (brokenImageResource != null) {
                         imageResource = brokenImageResource
                     }
                 }
@@ -129,34 +59,40 @@ fun ImageView.bindUri(
 
 fun ImageView.bindUri(
         uriObservable: ObservableProperty<String?>,
-        noImageResource: Int,
-        brokenImageResource: Int,
+        noImageResource: Int? = null,
+        brokenImageResource: Int? = null,
         imageMaxWidth: Int = 2048,
         imageMaxHeight: Int = 2048,
-        downloadRequest: NetRequest = NetRequest(NetMethod.GET, ""),
+        requestBuilder: Request.Builder = Request.Builder(),
         loadingObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false)
 ) {
     lifecycle.bind(uriObservable) { uri ->
         if (uri == null || uri.isEmpty()) {
             //set to default image
-            imageResource = noImageResource
+            if (noImageResource != null) {
+                imageResource = noImageResource
+            }
         } else {
             val uriObj = Uri.parse(uri)
             if (uriObj.scheme.contains("http")) {
                 loadingObs.value = (true)
-                imageStreamExif(context, downloadRequest.copy(url = uri), imageMaxWidth, imageMaxHeight, brokenImageResource = brokenImageResource) { disposer ->
+                requestBuilder.url(uri).lambdaBitmapExif(context, imageMaxWidth, imageMaxHeight).invokeAsync {
                     loadingObs.value = (false)
-                    if (disposer == null) {
+                    if (it.result == null) {
                         //set to default image or broken image
-                        imageResource = brokenImageResource
+                        if (brokenImageResource != null) {
+                            imageResource = brokenImageResource
+                        }
+                    } else {
+                        imageBitmap = it.result
                     }
                 }
             } else {
                 loadingObs.value = (true)
-                imageLocalUri(uriObj, imageMaxWidth, imageMaxHeight, brokenImageResource) { disposer ->
-                    loadingObs.value = (false)
-                    if (disposer == null) {
-                        //set to default image or broken image
+                try {
+                    imageURI = Uri.parse(uri)
+                } catch(e: Exception) {
+                    if (brokenImageResource != null) {
                         imageResource = brokenImageResource
                     }
                 }
