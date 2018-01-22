b1c8e01939770fb03bbf9f772f3a95771d86d1fc
Joseph Ivie <josephivie@gmail.com>
2018-01-22 11:45:56 -0700

    Deprecating updates

--- src/main/java/com/lightningkite/kotlin/anko/full/VCContainer.ext.kt
+++
@@ -1,9 +0,0 @@
-package com.lightningkite.kotlin.anko.full
-
-import com.lightningkite.kotlin.anko.viewcontrollers.ViewController
-import com.lightningkite.kotlin.anko.viewcontrollers.containers.VCContainer
-import com.lightningkite.kotlin.observable.property.ObservableProperty
-import com.lightningkite.kotlin.observable.property.transform
-
-val VCContainer.currentObs: ObservableProperty<ViewController>
-    get() = this.transform { it as ViewController }
