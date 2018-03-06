e6394a5a46b57b10e64b1f1aa2a456dc7ef4c53f
Joseph Ivie <josephivie@gmail.com>
2018-03-06 13:47:49 -0700

    Back to iBeacons

--- src/main/java/com/lightningkite/kotlin/anko/full/Networking.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/full/Networking.ext.kt
@@ -1,15 +1,24 @@
 package com.lightningkite.kotlin.anko.full
 
+import android.app.Activity
 import android.content.Context
 import android.view.View
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
+import com.lightningkite.kotlin.anko.getActivity
 import com.lightningkite.kotlin.anko.lifecycle
+import com.lightningkite.kotlin.anko.snackbar
+import com.lightningkite.kotlin.anko.viewcontrollers.dialogs.infoDialog
 import com.lightningkite.kotlin.anko.viewcontrollers.dialogs.standardDialog
 import com.lightningkite.kotlin.async.doUiThread
+import com.lightningkite.kotlin.networking.MyGson
+import com.lightningkite.kotlin.networking.TypedResponse
 import com.lightningkite.kotlin.observable.property.MutableObservableProperty
 import com.lightningkite.kotlin.observable.property.ObservableProperty
 import com.lightningkite.kotlin.observable.property.StandardObservableProperty
 import com.lightningkite.kotlin.observable.property.bind
-import org.jetbrains.anko.progressBar
 
 @Deprecated("Use the version in kotlin-observable instead.", replaceWith = ReplaceWith("captureProgress(observable)", "com.lightningkite.kotlin.observable"))
 fun <T> (() -> T).captureProgress(observable: MutableObservableProperty<Boolean>): (() -> T) {
@@ -74,5 +83,83 @@ fun <T> (() -> T).captureProgressInDialog(context: Context, title: Int? = null, 
             runningObs.value = false
         }
         response
+    }
+}
+
+fun <T> (() -> TypedResponse<T>).captureFailureInSnackbar(view: View, genericError: Int): () -> TypedResponse<T> {
+    return {
+        val response = this.invoke()
+        if (!response.isSuccessful()) {
+            doUiThread {
+                view.snackbar(response.toHumanStringError(view.resources.getString(genericError)))
+            }
+        }
+
+        response
+    }
+}
+
+fun <T> (() -> TypedResponse<T>).captureFailureInDialog(view: View, errorTextResource: Int, genericError: Int): () -> TypedResponse<T> = captureFailureInDialog(view.getActivity(), errorTextResource, genericError)
+
+fun <T> (() -> TypedResponse<T>).captureFailureInDialog(activity: Activity?, errorTextResource: Int, genericError: Int): () -> TypedResponse<T> {
+    return {
+        val response = this.invoke()
+        if (!response.isSuccessful()) {
+            doUiThread {
+                activity?.infoDialog(
+                        activity.resources.getString(errorTextResource),
+                        response.toHumanStringError(activity.resources.getString(genericError))
+                )
+            }
+        }
+
+        response
+    }
+}
+
+fun JsonElement.toHumanString(modifier: (String) -> String? = { it }, combiningString: String = "\n"): String {
+    var error: String = ""
+    when (this) {
+        is JsonPrimitive -> {
+            error = if (this.isString)
+                this.asString
+            else
+                this.toString()
+
+        }
+        is JsonObject -> {
+            error = this.entrySet().map {
+                val key = modifier(it.key)
+                val value = modifier(it.value.toHumanString(modifier, combiningString))
+                if (key == null) value
+                else if (value == null) key
+                else "$key: $value"
+            }.joinToString(combiningString)
+        }
+        is JsonArray -> {
+            error = this.map { it.toHumanString(modifier, combiningString) }.mapNotNull(modifier).joinToString(combiningString)
+        }
+    }
+    return error
+}
+
+fun JsonElement.toHumanStringError() = toHumanString(modifier = {
+    if (it.firstOrNull()?.isUpperCase() ?: false) it else null
+})
+
+fun TypedResponse<*>.toHumanStringError(fallback: String): String {
+    if (errorBytes == null) return fallback
+    return try {
+        errorBytes?.toString(Charsets.UTF_8)?.toHumanStringError(fallback) ?: fallback
+    } catch (e: Exception) {
+        fallback
+    }
+}
+
+fun String.toHumanStringError(fallback: String): String {
+    return try {
+        MyGson.json.parse(this).toHumanStringError()
+    } catch (e: Exception) {
+        fallback
     }
 }
